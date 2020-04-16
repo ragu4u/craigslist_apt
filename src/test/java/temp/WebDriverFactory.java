71a22bd77839432ed5ac9ec96e7c8853e2104d71
Raghou <raghav4code@gmail.com>
2020-04-16 16:27:36 -0700

    fix the bug which prevented us from creating a driver.

--- src/test/java/temp/WebDriverFactory.java
+++ src/test/java/temp/WebDriverFactory.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverFactory {
-    {
+    static {
         setDriverLocation(Driver.CHROME);
     }
 
