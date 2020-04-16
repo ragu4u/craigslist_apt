6b2a1b7eb6d8c9483ad33fe9b337d444e293824c
Raghou <raghav4code@gmail.com>
2020-04-16 16:48:20 -0700

    added settings for chrome browser.

--- src/test/java/temp/WebDriverFactory.java
+++ src/test/java/temp/WebDriverFactory.java
@@ -2,6 +2,9 @@ package temp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Collections;
 
 public class WebDriverFactory {
     private static final String DRIVER_FOLDER = "C:/work/installations/selenium_drivers";
@@ -15,7 +18,7 @@ public class WebDriverFactory {
 
         switch (driverName){
             case CHROME:
-                driver = new ChromeDriver();
+                driver = new ChromeDriver(getChromeOptions());
                 break;
             default:
                 throw new UnsupportedOperationException("The browser " + driverName + " is not supported.");
@@ -26,6 +29,18 @@ public class WebDriverFactory {
     private static void setDriverLocations(){
         //Chrome.
         System.setProperty("webdriver.chrome.driver", DRIVER_FOLDER + "/chromedriver.exe");
+    }
+
+    private static ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--start-maximized");
+
+        //Next two lines to remove the annoying info bar on top of the browser.
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+        options.setExperimentalOption("useAutomationExtension", false);
+
+        return options;
     }
 
 }
