204927ad8474ed556f644f1f2559985da1842837
Raghou <raghav4code@gmail.com>
2020-04-17 00:09:48 -0700

    clean imports.

--- src/test/java/temp/ApartmentSearchOptions.java
+++ src/test/java/temp/ApartmentSearchOptions.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ApartmentSearchOptions {
     private final WebDriver driver;
